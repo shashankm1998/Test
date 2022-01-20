d1208e77930be381b605b57d19740f8bf97fbfd5
“[Shashank <shashank.m998@gamil.com>
2022-01-20 16:34:16 +0530

    Initial commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CoinsTable from "../components/CoinsTable";
+
+const Homepage = () => {
+  return (
+    <>
+      <CoinsTable />
+    </>
+  );
+};
+
+export default Homepage;
